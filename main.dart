fe3a180eab6fb6492a73bc865e96775ff4af80d6
Nurhidayanti19 <100064899+Nurhidayanti19@users.noreply.github.com>
2022-03-15 18:15:14 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    new MaterialApp(
+      title: "My Apps",
+      home: new Myapp(),
+    )
+  );
+}
+
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("My Apps"),
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(20.0),
+        child: new Center(
+          child: new Text("perkenalkan saya Nurhidayanti"),
+        )
+      )
+    );
+  }
+}
